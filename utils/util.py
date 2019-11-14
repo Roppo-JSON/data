1ca0680ccd82e4b1a3e0054ea8f26725c824992e
OldBigBuddha <development@oldbigbuddha.net>
2019-11-14 12:52:43 +0900

    [update] util.py
    Add remove_ruby
    Add get_example

--- utils/util.py
+++ utils/util.py
@@ -19,6 +19,41 @@ def get_text_from_paragraph_sentence(paragraph_sentence: list) -> str:
     sentences: list = become_list(paragraph_sentence['Sentence'])
     text: list = []
     for sentence in sentences:
-        text.append(sentence['#text'])
+        if 'Ruby' in sentence:
+            text.append(remove_ruby(sentence))
+        else:
+            text.append(sentence['#text'])
 
     return ''.join(text)
+
+
+def remove_ruby(sentence: dict) -> str:
+    """Ruby が存在する Sentence 内の条文を、ルビが振られている漢字を文中に含めた文字列を返す"""
+
+    rubies = become_list(sentence['Ruby'])
+    texts = sentence['#text']
+
+    # ルビが振られている漢字も含めた条文
+    complete_text: list = []
+
+    for i, text in enumerate(texts):
+        complete_text.append(text)
+        if i < len(rubies):
+            complete_text.append(rubies[i]['#text'])
+
+    return ''.join(complete_text)
+
+
+def get_examples(items: list) -> list:
+    """例をリストとして取得する"""
+
+    examples: list = []
+    for item in items:
+        examples.append(get_text_from_paragraph_sentence(item['ItemSentence']))
+
+    return examples
+
+
+def print_line(length=20) -> str:
+    """区切り線の表示"""
+    print('=' * length)
